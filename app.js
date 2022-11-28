4a21747b6e854bf40178b2f6eca3fa6b6dab145c
dkartes <hetyei.kevin@gmail.com>
2022-11-28 15:24:30 +0100

    mise a jour des controllers + routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("dotenv").config({ path: "./config/.env" });
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+
 const app = express();
 
 //initialisation du port
@@ -21,3 +23,22 @@ mongoose
   )
   .then(() => console.log("Connexion a MongoDB réussie"))
   .catch(() => console.log("Echec de la connexion a MongoDB"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
